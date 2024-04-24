4460fdcc2d4778cdf71a60059eddb523e945b43e
t1mmyboy <99074833@mydavinci.nl>
2024-04-24 12:11:51 +0200

    laravel installation

---
+++ app/Http/Controllers/yippieController.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Http\Controllers;
+use App\Http\Controllers\yippieController;
+
+class yippieController extends Controller{
+    public function music(){
+        return view("yip");
+    }
+}
+?>
